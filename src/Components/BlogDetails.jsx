495923c922f97dd2ef6c2b9d2546b84f693a9e62
Abdur Rehman <abdurrehmansaleem5@gmail.com>
2022-09-07 16:45:49 +0500

    Update

--- src/Components/BlogDetails.jsx
+++ src/Components/BlogDetails.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Blog from "./Blog";
 import { useParams, useNavigate } from "react-router-dom";
-import useDelete from "./useDelete";
 import useFetch from "./useFetch";
 import Loader from "./Loader";
 function BlogDetails() {
